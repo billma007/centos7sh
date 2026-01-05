937d58826314e82291d46ea0cb0b5abc5256f3f6
Bill Ma <3234301011@stmail.ujs.edu.cn>
2026-01-05 18:41:48 +0800

    Update yum.sh to configure new yum repositories
    Backup original yum repository, download new CentOS and EPEL repositories, and add SCLo sources with GPG key import.

--- yum.sh
+++ yum.sh
@@ -1,21 +1,51 @@
+
 #!/bin/bash
+
+# 备份原 yum 源
 mv /etc/yum.repos.d /etc/yum.repos.d.bak
 mkdir /etc/yum.repos.d
-curl -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.cloud.tencent.com/repo/centos7_base.repo
-yum clean all
-yum makecache
-curl -O /etc/yum.repos.d/epel.repo http://mirrors.cloud.tencent.com/repo/epel-7.repo
-yum clean all
-yum makecache
-echo -e "[centos-sclo-sclo]\nname=CentOS-\$releasever - SCLo sclo\nbaseurl=https://mirrors.cloud.tencent.com/centos/\$releasever/sclo/x86_64/sclo/\ngpgcheck=1\nenabled=1\ngpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7" | sudo tee /etc/yum.repos.d/CentOS-SCLo-scl.repo
-echo -e "[centos-sclo-rh]\nname=CentOS-\$releasever - SCLo rh\nbaseurl=https://mirrors.cloud.tencent.com/centos/\$releasever/sclo/x86_64/rh/\ngpgcheck=1\nenabled=1\ngpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7" | sudo tee /etc/yum.repos.d/CentOS-SCLo-scl-rh.repo
-yum clean all
-yum makecache
-cd /etc/yum.repos.d
-rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-SIG-SCLo
-rpm -qi gpg-pubkey-f2ee9d55
+
+# 下载 CentOS 官方基础源
+curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.cloud.tencent.com/repo/centos7_base.repo
 yum clean all
 yum makecache
 
+# 下载 EPEL 源
+curl -o /etc/yum.repos.d/epel.repo http://mirrors.cloud.tencent.com/repo/epel-7.repo
+yum clean all
+yum makecache
+
+# 添加 SCLo sclo 源
+cat <<EOF | tee /etc/yum.repos.d/CentOS-SCLo-scl.repo
+[centos-sclo-sclo]
+name=CentOS-\$releasever - SCLo sclo
+baseurl=https://mirrors.cloud.tencent.com/centos/\$releasever/sclo/x86_64/sclo/
+gpgcheck=1
+enabled=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+EOF
+
+# 添加 SCLo rh 源
+cat <<EOF | tee /etc/yum.repos.d/CentOS-SCLo-scl-rh.repo
+[centos-sclo-rh]
+name=CentOS-\$releasever - SCLo rh
+baseurl=https://mirrors.cloud.tencent.com/centos/\$releasever/sclo/x86_64/rh/
+gpgcheck=1
+enabled=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+EOF
+
+yum clean all
+yum makecache
+
+# 导入 SCLo GPG key
+cd /etc/yum.repos.d
+rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-SIG-SCLo
+
+# 验证公钥是否已安装
+rpm -qi gpg-pubkey-f2ee9d55
+
+yum clean all
+yum makecache
 
 echo "done!"
